11286b7b112b45b44501c0d6ccc1ff8b8ae0377e
bclayton314 <62265654+bclayton314@users.noreply.github.com>
2022-05-29 19:26:14 -0500

    Add files via upload

---
+++ Reciever.java
@@ -0,0 +1,191 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Random;
+//import java.util.Scanner;
+/* 
+ * Reciever.java 
+ * 
+ * Revisions: Initial Version
+ *    
+ */
+/**
+ * The Reciever class handles all the functinalities in the Reciever side
+ * of a RDT 3.0 protocol.
+ *
+ * @author Sanket Agarwal
+ */
+
+public class Reciever{
+	DatagramSocket socket=null;
+	DatagramPacket receivePacket = null;
+	DatagramPacket sendpacket = null;
+	int port;
+	InetAddress address=null;
+	int seq=0;
+	static String ack="ACK";
+	private static byte[] out_data;
+	private static byte[] in_data;
+
+	public Reciever(){
+		/*
+		 * Initializing the datgram Socket object at port 8082
+		 */
+		try {
+			socket = new DatagramSocket(8082);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void rdt_recieve(){
+		/*
+		 * This method actually handles all the receiving of packets
+		 * and sending of acknowldgements plus depicting of few losing scenarios.
+		 */
+		out_data = new byte[1024];
+		in_data = new byte[1024];
+		ObjectOutputStream out;
+		
+		boolean corrupted=false; // Keeping track of if a packet is corrupted
+		int c=0;
+		try{
+			while(true){ // running infinitely
+				
+				corrupted=false;
+				receivePacket = new DatagramPacket(in_data, in_data.length);
+				int timeout = 2010;  // timeout duration 4010 milli-secs
+
+				socket.receive(receivePacket); // receiving packet from sender
+				
+				byte[] rcv_stream = receivePacket.getData();
+				ByteArrayInputStream bs = new ByteArrayInputStream(rcv_stream);
+				ObjectInputStream out_s = new ObjectInputStream(bs);
+				Packet pkt = (Packet) out_s.readObject();
+				String data = pkt.get_payLoad();
+
+				String ckhsum_rcvd = pkt.get_checkSum();
+				int seq_rcvd = pkt.get_seq();
+				
+				if(seq_rcvd==seq && c!=0){
+					System.out.println("--------Detect duplicate packet----------)");
+					c--; // Ignoring the duplicate
+				}
+				checkSum cksm = new checkSum();  // Initializing the checkSum class for checkSum calculation
+				
+				// calculating the checksum (just the sum) for seeing if packet is corrupted
+				String chksum_claculated = cksm.cal_sum(data.getBytes()); 
+				// adding the cheksums
+				String sum_of_both = cksm.binAddition(ckhsum_rcvd, chksum_claculated);
+				//System.out.println("Sum of the the two checksums "+sum_of_both);
+				
+				seq = seq_rcvd;  // setting sequence number to be sent for acknowledgement to the current acknowledged packet sequence number
+				for(int i=0;i<sum_of_both.length();i++){
+					if(sum_of_both.charAt(i)!='1'){
+						if(seq_rcvd == 0)seq=1; // If packet is corrupted send the sequence no. of the previous packet
+						else seq = 0;
+						corrupted=true;
+
+						System.out.println("packet "+c + " received corrupted");	
+						System.out.println("resend ACK for packet "+(c-1));
+						c--;
+						break;
+					}
+				}
+
+
+				Packet pkt_obj = make_pkt(seq); // making the ACK packet
+				address = receivePacket.getAddress();
+				port = receivePacket.getPort();
+
+				ByteArrayOutputStream bo = new ByteArrayOutputStream();
+				out = new ObjectOutputStream(bo);
+				out.writeObject(pkt_obj);
+				out.flush();
+				out_data = bo.toByteArray();
+				
+				// creating the Datagram packet to be sent
+				sendpacket = new DatagramPacket(out_data, out_data.length,address,port);
+				Random randomno = new Random();
+				// generating random number for depicting the ACK lost and delaying packet scenario
+				int randomEvent = randomno.nextInt(20); 
+
+				if(randomEvent==10){  // depicting premature time out by delaying the ACK sent
+					Thread.sleep(timeout);  
+					System.out.println("----Depicting premature timeout----");
+				}
+				if(seq_rcvd==seq){
+					System.out.println("packet "+c + " received correctly");
+					System.out.println("recieved payload "+pkt.get_payLoad());
+					System.out.println("recieved sequence "+pkt.get_seq());
+					System.out.println("send ACK for packet "+c);
+					System.out.println("--------------------------------------------------");
+					System.out.println();
+				}
+				// Not sending ACK to depict the ACL lost scenario
+				if(randomEvent==11)
+					System.out.println("Lost ACK");
+				if(randomEvent!=11 && !corrupted){ 
+					socket.send(sendpacket);
+				}
+				
+
+				c++; // Increasing the number of counter
+			}
+
+
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			socket.close();
+		}
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			socket.close();
+		}
+		catch(InterruptedException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			socket.close();
+		}
+		socket.close();
+
+	}
+
+	public static Packet make_pkt(int seq){
+
+		Packet snd_pkt = new Packet(null,null,seq,ack);
+		return snd_pkt;
+
+	}
+	public  void closeSocket(){
+		socket.close();
+		System.exit(1);
+	}
+	public static void main(String[] args){
+		Reciever r = new Reciever();
+		System.out.println("-------------Receiver Screen--------------");
+		System.out.println("*******************************************");
+		System.out.println("*******************************************");
+		System.out.println("*******************************************");
+		System.out.println("*******************************************");
+		r.rdt_recieve();
+		/*int exit;
+		Scanner user_input = new Scanner( System.in );
+		exit = user_input.nextInt();
+		if(exit==1)
+			user_input.close();
+			r.closeSocket();*/
+		
+	}
+
+
+}
---
+++ Sender.java
@@ -0,0 +1,269 @@
+import java.util.Random;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+//import java.util.Scanner;
+/* 
+ * Sender.java 
+ * 
+ * Revisions: Initial Version
+ *    
+ */
+/**
+ * The Sender class handles all the functinalities in the Sender side
+ * of a RDT 3.0 protocol.
+ *
+ * @author Sanket Agarwal
+ */
+
+public class Sender{
+
+	private static byte[] out_data;
+	private static byte[] in_data;
+	protected DatagramSocket socket = null;
+	InetAddress address=null;
+	DatagramPacket send_packet=null;
+	DatagramPacket receive_Packet = null;
+
+
+	public Sender(){
+		/*
+		 * Initislizing the datagram socket and making the address object
+		 */
+		try {
+			socket = new DatagramSocket(); 
+
+			address = InetAddress.getByName("localhost");	
+		}
+		catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		catch (SocketException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+	public  void udt_send(int c, int seq,Packet pkt_obj,int timeout){  
+		/*
+		 * This method just sends the datagram packet to the reciever port
+		 */
+		ObjectOutputStream out;
+		try{
+			byte[] outBuf = new byte[1024];
+			ByteArrayOutputStream bo = new ByteArrayOutputStream();
+			out = new ObjectOutputStream(bo);
+			out.writeObject(pkt_obj);
+			out.flush();	
+			outBuf = bo.toByteArray();
+			send_packet = new DatagramPacket(outBuf, outBuf.length, address, 8082);
+
+			socket.send(send_packet);
+			// setting the socket for timeout after a certain time
+			socket.setSoTimeout(timeout); 
+		}
+		catch (NotSerializableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			socket.close();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			socket.close();
+		}
+	}
+
+	public Packet rdt_recieve(){
+		/*
+		 * This method just recieves ACK until a certain amount of timeout time
+		 */
+		in_data = new byte[1024];
+		Packet pkt = null;
+		try{
+			receive_Packet = new DatagramPacket(in_data, in_data.length);
+			socket.receive(receive_Packet);
+			byte[] rcv_stream = receive_Packet.getData();
+			ByteArrayInputStream bs = new ByteArrayInputStream(rcv_stream);
+			ObjectInputStream in_s = new ObjectInputStream(bs);
+			pkt = (Packet) in_s.readObject();
+
+		}
+		catch (SocketTimeoutException e) {
+			/*
+			 * If the timeout happens control comes in this 
+			 * catch block.
+			 * It just returns Null
+			 */
+			return pkt;
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			socket.close();
+		}
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			socket.close();
+		}
+		return pkt;
+	}
+	
+	public void send_simulator(int c, int seq,Packet pkt_obj){
+		/*
+		 * Medium loss simulator
+		 */
+		Random randomno = new Random();
+		int timeout = 2000;
+		// Generating random numbers for random loss and corruption depiction
+		int randLoss = randomno.nextInt(15);
+		
+		if(randLoss==6){
+			// doesn't send tha packet at all
+			System.out.println();
+			System.out.println();
+			System.out.println("----------------------------------------------------");
+			System.out.println("sending packet "+c);
+			System.out.println("sending packet "+pkt_obj.get_payLoad());
+			System.out.println("Sending sequence "+pkt_obj.get_seq());
+			System.out.println("Packet "+c+" is lost");
+		}
+		if(randLoss!=6 && randLoss !=10){
+			//Normal sending of the packet
+			System.out.println();
+			System.out.println();
+			System.out.println("----------------------------------------------------");
+			System.out.println("sending packet "+c);
+			System.out.println("sending packet "+pkt_obj.get_payLoad());
+			System.out.println("Sending sequence "+pkt_obj.get_seq());
+			udt_send(c, seq, pkt_obj,timeout);
+
+		}
+		else if(randLoss==10){
+			System.out.println();
+			System.out.println();
+			System.out.println("----------------------------------------------------");
+			System.out.println("sending packet "+c);
+			System.out.println("sending packet payload"+pkt_obj.get_payLoad());
+			System.out.println("Sending sequence "+pkt_obj.get_seq());
+			System.out.println("corrupting packet"+ c);
+			pkt_obj.corruptPayload();  //Corrupting the payload
+			udt_send(c, seq, pkt_obj,timeout);
+			pkt_obj.correctPayload();  //correcting it for sending it again
+		}
+
+	}
+	
+	public void udt_send_runner(){
+		/*
+		 * This is the main method which is running
+		 * an infinite loop for sending packets and recieving ACKs
+		 */
+		int c = 0;  // Packet number
+		int seq = 0;  //sequence number of packet 
+		int prev_seq = 1;
+		checkSum chk = new checkSum();
+		boolean delayed = false;
+		String checksum=null;
+		String data = null;
+		Packet pkt_obj=null;
+		Packet pkt = null;
+		while(true){ // Infinite loop
+			out_data = new byte[1024];
+			
+			if(!delayed){ // Executes only if original pkt to be sent
+				data = dataGenerator(); // getting data to be sent from upper layer depiction
+				out_data = data.getBytes();
+				checksum = chk.make_cheksum(out_data);
+				pkt_obj = make_pkt(seq,data,checksum);
+			}
+			
+			delayed=false;
+			// sending the packet to the sending simulator to in turn send to the reciever
+			send_simulator(c, seq, pkt_obj); 
+			pkt = rdt_recieve();
+			
+			//Premature timeout checking
+			if(pkt !=null && pkt.get_seq()==prev_seq && pkt.get_seq() != pkt_obj.get_seq()){ 
+
+				//If premature timeout do nothing on the acknowledgement received again for the same sequence
+				pkt = rdt_recieve(); //receiving the ACK for currently sent packet and do nothing
+			}
+			
+			if(pkt==null){ // timeout
+				System.out.println("timeout for packet "+c);
+				delayed = true;
+				c--; // resending the same packet again
+				seq = (seq^1); // toggling 0 and 1
+			}
+
+			else {
+				System.out.println("recieved ACK for packet "+c);
+			}
+
+			c++;
+			prev_seq = seq; // Storing previously acked sequence
+			seq = (seq^1); // Toggling between 0 and 1
+		}
+
+	}
+
+	public static String dataGenerator(){ 
+		/*
+		 * This method generates data to be sent to the 
+		 * network. It depicts the Application layer
+		 */
+		String[] data = {"hello","day","mango","apple","star","moon","banana","rose","RIT","Sanket","234"};
+		
+		// Random number to select data randomly from the data list
+		Random randomno = new Random(); 
+		return data[randomno.nextInt(data.length)];
+	}
+
+
+	public static Packet make_pkt(int seq, String data, String checksum){
+		/*
+		 * This mathod calls the Packet construvtor and makes the Packet to be sent
+		 */
+		Packet snd_pkt = new Packet(data,checksum,seq,null);
+		return snd_pkt;
+
+	}
+	public  void closeSocket(){
+		socket.close();
+		System.exit(1);
+	}
+
+	public static void main(String[] args){
+		/*
+		 * This is the main method
+		 */
+		Sender s = new Sender();
+		System.out.println("-------------Sender Screen--------------");
+		System.out.println("*******************************************");
+		System.out.println("*******************************************");
+		System.out.println("*******************************************");
+		System.out.println("*******************************************");
+		s.udt_send_runner(); // calling the sending loop method
+		/*int exit;
+		Scanner user_input = new Scanner( System.in );
+		exit = user_input.nextInt();
+		if(exit==1)
+			user_input.close();
+			s.closeSocket();*/
+	}
+
+
+
+}
